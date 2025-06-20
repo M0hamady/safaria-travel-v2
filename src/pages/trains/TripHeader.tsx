030f6ab269fd87770bc31da3e9b6b3d14644a43c
M0hamady <m0ham3dy96@gmail.com>
2025-06-20 16:54:59 +0300

    upgrade

--- src/pages/trains/TripHeader.tsx
+++ src/pages/trains/TripHeader.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { TrainsContext } from "../../context/TrainsContext";
 import images from "../../assets";
 import { useTranslation } from "react-i18next";
+import { PinDrop } from "@mui/icons-material";
 
 export const TripHeader = () => {
   const { selectedTrip, selectedArrivalLocation,selectedArrivalStation,selectedDepartureLocation,selectedDepartureStation } = useContext(TrainsContext);
@@ -13,7 +14,7 @@ export const TripHeader = () => {
 
 
   return (
-    <header className="bg-primary text-white pt-2 px-8 flex flex-col capitalize items-center justify-center">
+    <header className="bg-primary text-white pt-2 px-8 flex flex-col capitalize items-center justify-center w-full max-sm:justify-start max-sm:items-start">
       {/* Back Button */}
       <div className="w-full flex justify-start items-center mb-3">
         <button
@@ -42,18 +43,26 @@ export const TripHeader = () => {
       </div>
 
       {/* Trip Route */}
-      <div className="flex gap-5 justify-center items-center translate-y-8 mx-auto max-sm:flex-col max-sm:justify-start max-sm:items-start ">
-        <div className="text-lg font-semibold w-fit  text-right">
+      <div className="flex gap-5 justify-center w-full items-center translate-y-8 mx-auto max-sm:flex-col max-sm:justify-start max-sm:items-start ">
+        <div className="text-lg font-semibold w-fit  text-right max-sm:flex max-sm:flex-col max-sm:justify-start">
+          <span className="md:hidden"><PinDrop /> {t("busSearchTrips.departureStation")}: </span>
+          <span className="max-sm:text-lg max-sm:text-start ltr:max-sm:ml-8 rtl:max-sm:mr-8 ">
+
           {selectedDepartureStation?.name}({selectedDepartureLocation?.name})
+          </span>
         </div>
 
-        <div className="flex items-center max-sm:items-start  max-sm:rotate-90 max-sm:translate-x-16  justify-center max-sm:justify-start  ">
+        <div className="flex items-center max-sm:items-start  max-sm:rotate-90 max-sm:translate-x-16  justify-center max-sm:justify-start max-sm:hidden   ">
         <img src={images.railWay}  className="w-28 h-4 object-cover  max-sm:w-20 " />
 
         </div>
 
-        <div className="text-lg font-semibold  text-left">
+        <div className="text-lg font-semibold w-fit  text-right max-sm:flex max-sm:flex-col max-sm:justify-start max-sm:mb-8">
+          <span className="md:hidden"><PinDrop /> {t("busSearchTrips.arrivalStation")}: </span>
+          <span className="max-sm:text-lg max-sm:text-start ltr:max-sm:ml-8 rtl:max-sm:mr-8  ">
+
         {selectedArrivalStation?.name}({selectedArrivalLocation?.name})
+          </span>
         </div>
       </div>
 
--- src/pages/trains/TripListItem.tsx
+++ src/pages/trains/TripListItem.tsx
@@ -49,12 +49,12 @@ const TripListItem: React.FC<Props> = ({ trip, onSelect, getMinPrice }) => {
           <h3 className="font-semibold text-lg max-sm:ltr:text-left  flex md:gap-3 max-sm:justify-between max-sm:w-full"><span className='w-full text-[0.85rem] flex ' > {t("train_number")}: </span> <span className='w-fit text-[1.0rem]' > {trip.train.name}</span> </h3>
           {/* we will add container has routes of trip each route has id and name while hover preview list in drobdown miu=nue  */}
           <div className="relative group">
-            <h3 className="font-semibold text-lg max-sm:ltr:text-left  px-2  max-sm:pl-0 max-sm:pr-5  py-2 rounded text-primary underline flex">
+            <h3 className="font-semibold text-lg max-sm:ltr:text-left  px-2  ltr:max-sm:pl-0 ltr:max-sm:pr-5 rtl:max-sm:pr-0 rtl:max-sm:pl-5  py-2 rounded text-primary underline flex">
               <span className='min-w-full'>{t("train_route")}  </span><span> <AltRoute/> </span>
             </h3>
 
             {/* Dropdown on hover */}
-            <div className="absolute max-sm:-left-[4.5rem] top-6 z-10 hidden group-hover:block bg-white  shadow-md border rounded-md mt-2 w-64 text-sm text-left rtl:text-right">
+            <div className="absolute ltr:max-sm:-left-[4.5rem] rtl:max-sm:-right-[4.5rem]  top-6 z-10 hidden group-hover:block bg-white  shadow-md border rounded-md mt-2 w-64 text-sm text-left rtl:text-right">
               <ul className="p-2 max-h-60 overflow-y-auto">
                 {trip.route && trip.route.length > 0 ? (
                   trip.route.map((route) => (
